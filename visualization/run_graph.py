7caff909b0423e24898f609023b00e67be3452ea
marcos <markisbcn@gmail.com>
2024-04-28 23:18:33 +0200

    initial graphing python script

---
+++ visualization/run_graph.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plot
+
+fig1 = plot.figure()
+axis1 = fig1.add_subplot(111)
+
+df = pd.read_csv("samples.csv")
+#df.head()
+
+df2 = df.copy()
+df2['temperature'] = df2['temperature'].transform(lambda x: x + 1.015*x)
+
+xName = 'seconds'
+yName = 'temperature'
+plot.plot(xName, yName, data=df, color='green', marker='+', linewidth=1, markersize=5, label='line1')
+plot.plot(xName, yName, data=df2, color='blue', marker='*', linewidth=1, markersize=5, label='line2')
+
+#plot.show()
+#plot.savefig("sampledata.png")
+plot.savefig("myImagePDF.pdf", format="pdf", bbox_inches="tight")
