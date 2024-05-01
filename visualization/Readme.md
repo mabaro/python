5c0854fbfd53792fb55e2fc42f7e87b0517827fc
marcos <markisbcn@gmail.com>
2024-05-01 21:55:26 +0200

    cleaning

--- visualization/Readme.md
+++ visualization/Readme.md
@@ -4,5 +4,13 @@
 python3 -m venv visenv
 # start environment (having access to environment installed packages)
 source visenv/bin/activate
+# edit venv with
+deactivate
+
+# install any packages needed
+pip install matplotlib pandas plotly dash jupyterlab
+# save requirements
+pip freeze > requirements.txt
+
 # enter local jupyter lab
 jupyter lab
---
+++ visualization/run_mydata.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plot
+
+# df = pd.read_csv("data/2023-binanceTransactionHistory.csv")
+# df.head()
+
+# df = pd.read_csv("data/2023-binanceTransactionHistory.csv")
+# df.head()
+
+from binance.client import Client
+import binance
+import os
+
+# api_key = os.environ.get('somekey1')
+# api_secret = os.environ.get('somekey2')
+
+# client = Client(api_key, api_secret)
+# print("account data")
+# print( client.get_account() )
+# df = pd.DataFrame.from_list( client.get_account() )
+# df = pd.read_json( client.get_account() )
+# df.head()
+# print("asset balance")
+# print(client.get_asset_balance(asset='BTC'))
