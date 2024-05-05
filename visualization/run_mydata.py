d6423eaf882bd3c4b7d43df648b1dd1dda9ecd59
marcos <markisbcn@gmail.com>
2024-05-05 18:03:44 +0200

    wip binance scripts

--- visualization/run_mydata.py
+++ visualization/run_mydata.py
@@ -1,24 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plot
 
-# df = pd.read_csv("data/2023-binanceTransactionHistory.csv")
-# df.head()
-
-# df = pd.read_csv("data/2023-binanceTransactionHistory.csv")
-# df.head()
-
 from binance.client import Client
 import binance
 import os
 
-# api_key = os.environ.get('somekey1')
-# api_secret = os.environ.get('somekey2')
+api_key = os.environ.get('somekey1')
+api_secret = os.environ.get('somekey2')
 
-# client = Client(api_key, api_secret)
-# print("account data")
-# print( client.get_account() )
-# df = pd.DataFrame.from_list( client.get_account() )
-# df = pd.read_json( client.get_account() )
-# df.head()
+client = Client(api_key, api_secret)
+print("account data")
+df = client.get_account()
+print("keys: ", df.keys())
+balances = df["balances"]
+df = pd.DataFrame.from_json( balances )
+df.head()
 # print("asset balance")
 # print(client.get_asset_balance(asset='BTC'))
