8b7d7eb6b9adf517ed5e1bb425111d0149e0dc26
Marcos Balsa <markisbcn@gmail.com>
2024-07-02 00:30:47 +0200

    added folder/file filtering

--- visualization/run_plotter.py
+++ visualization/run_plotter.py
@@ -12,6 +12,7 @@ import os
 
 app = Dash(external_stylesheets=[dbc.themes.SLATE], prevent_initial_callbacks="initial_duplicate")
 
+baseDataPath = ""
 sourceData = []
 dataFrames = []
 dataFrameIndex = 0
@@ -33,12 +34,18 @@ tableViewConfig = {
        )          
 def updateXAxis(search_value, options):
     global xAxis
+
+    changed_inputs = [
+        x["prop_id"]
+        for x in callback_context.triggered
+    ]
+    if "xaxis.options" in changed_inputs:
+        raise PreventUpdate
     
-    if not search_value:
+    if search_value is None:
         raise PreventUpdate
 
-    print("updateXAxis search_value: ", search_value)
-    print("updateXAxis options: ", options)
+    xAxis = None
 
     for o in options:
         if search_value == o["value"]:
@@ -57,19 +64,24 @@ def updateXAxis(search_value, options):
 def updateColumns(search_value, options):
     global dataColumns
     
-    if not search_value:
+    changed_inputs = [
+        x["prop_id"]
+        for x in callback_context.triggered
+    ]
+    if "columns.options" in changed_inputs:
         raise PreventUpdate
 
-    print("updateColumns search_value: ", search_value)
-    print("updateColumns options: ", options)
+    if search_value is None:
+        raise PreventUpdate
 
     if not isinstance(search_value, list):
         search_value = [search_value]    
 
     dataKeys = dataFrames[dataFrameIndex].keys()
-    dataColumns = []
-    for key in search_value:
-        dataColumns.append(dataKeys[key])
+    dataColumns = [dataKeys[key] for key in (search_value or [])]
+
+    if len(dataColumns) == 0:
+        dataColumns = [k for k in dataKeys if k != xAxis ]
 
     return drawFigure(dataFrames[dataFrameIndex])
 
@@ -77,36 +89,38 @@ def drawFigure(tableData):
     global xAxis
     global dataColumns
 
-    print("drawFigure X: ", xAxis)
-    print("drawFigure columns: ", dataColumns)
+    if tableData is None:
+      fig = go.Figure()
+    else:    
+        fig=px.line(
+            tableData, 
+            x=xAxis,
+            y=dataColumns,
+            # color="fps",
+            # text = "fps",
+            markers = True,
+            # labels = {'seconds':'time', 'gpu' : 'GPU freq.' },
+            # hover_data="gpu",
+        ).update_layout(
+            template='plotly_dark',
+            plot_bgcolor= 'rgba(0, 0, 0, 0)',
+            paper_bgcolor= 'rgba(0, 0, 0, 0)',
+            # hovermode="x unified",
+            hoverlabel=dict(
+                bgcolor="black",
+                font_size=14,
+                font_family="Rockwell"
+            ),
+        ).update_traces(
+            textposition="bottom right",
+            # hovertemplate = "Fps:%{fpsColumn}: <br>Temperature: %{y}",
+        )
 
     return html.Div([
         dbc.Card(
             dbc.CardBody([
                 dcc.Graph(
-                    figure=px.line(
-                        tableData, 
-                        x=xAxis,
-                        y=dataColumns,
-                        # color="fps",
-                        # text = "fps",
-                        markers = True,
-                        # labels = {'seconds':'time', 'gpu' : 'GPU freq.' },
-                        # hover_data="gpu",
-                    ).update_layout(
-                        template='plotly_dark',
-                        plot_bgcolor= 'rgba(0, 0, 0, 0)',
-                        paper_bgcolor= 'rgba(0, 0, 0, 0)',
-                        # hovermode="x unified",
-                        hoverlabel=dict(
-                            bgcolor="black",
-                            font_size=14,
-                            font_family="Rockwell"
-                        ),
-                    ).update_traces(
-                        textposition="bottom right",
-                        # hovertemplate = "Fps:%{fpsColumn}: <br>Temperature: %{y}",
-                    ),
+                    figure = fig,
                     config={
                         'displayModeBar': False
                     }
@@ -117,6 +131,13 @@ def drawFigure(tableData):
 
 def drawTable(tableData):
     DrawingTable=True
+
+    if tableData is None:
+        return dash_table.DataTable(
+            id='data-table',
+            data=[],
+            columns=[],)
+
     return dash_table.DataTable(
             id='data-table',
             data=tableData.to_dict("records"),
@@ -189,12 +210,16 @@ def RefreshPage() :
     global xAxis
     global dataColumns
 
-    DF = dataFrames[dataFrameIndex]
-    DF_KEYS = DF.keys() 
-    xAxis = DF_KEYS[0]
-    dataColumns = [DF_KEYS[1]]
-    print("RefreshPage X: ", xAxis)
-    print("RefreshPage columns: ", dataColumns)
+    DF = None
+    DF_KEYS = []
+    dataColumns = None
+    xAxis = ""
+
+    if len(dataFrames) > 0 :
+        DF = dataFrames[dataFrameIndex]
+        DF_KEYS = DF.keys()
+        xAxis = DF_KEYS[0]
+        dataColumns = [DF_KEYS[1]]
 
     return dbc.Container([
             dbc.Card(
@@ -223,13 +248,23 @@ def RefreshPage() :
                                 id="dataset-path",
                                 type="text",
                                 placeholder="Enter datasets path",
+                                debounce = True,
                             ),
                         ], width=3),
                         dbc.Col([
                             dcc.Input(
-                                id="dataset-filter",
+                                id="dataset-filename-filter",
                                 type="text",
-                                placeholder="Enter datasets filter(; to separate allowed substrings ! to negate)",
+                                placeholder="Enter filename filter(; to separate allowed substrings ! to negate)",
+                                debounce = True,
+                            ),
+                        ], width=2),
+                        dbc.Col([
+                            dcc.Input(
+                                id="dataset-dir-filter",
+                                type="text",
+                                placeholder="Enter folder filter(; to separate allowed substrings ! to negate)",
+                                debounce = True,
                             ),
                         ], width=2),
                     ]),
@@ -273,44 +308,98 @@ def RefreshPage() :
 
 @app.callback(
         Output("dataset-select", "options"), 
-        Input("dataset-path", "value"))
-def list_datasets(datasetsPath):
+        Input("dataset-path", "value"),
+        Input("dataset-filename-filter", "value"),
+        Input("dataset-dir-filter", "value"),
+        )
+def update_dataset_list(datasetsPath, filenameFilterStr, folderFilterStr):
     global dataFrames
     
+    if not datasetsPath:
+        raise PreventUpdate
+
+    filenameFilter = []
+    if isinstance( filenameFilterStr, str):
+        filenameFilter = filenameFilterStr.split(";")
+    folderFilter = []
+    if isinstance( folderFilterStr, str):
+        folderFilter = folderFilterStr.split(";")
+
     dataFrames = []
 
-    file_names = os.listdir(datasetsPath)
-    file_list = []
+    filtered_files = []
 
-    count = 0
-    for file in file_names:
-        if "csv" in file:
-            dataFrames.append( LoadData(filename) )
-            file_list.append({'label': sourceData[count], 'value': count} ),
-            count = count + 1
+    excludeDirs = [".venv"]
+    excludesDir = [ f[1:] for f in folderFilter if len(f)>0 and f[0] == "!" ]
+    includesDir = [ f for f in folderFilter if len(f)>0 and not f[0] == "!" ]
+    excludes = [ f[1:] for f in filenameFilter if len(f)>0 and f[0] == "!" ]
+    includes = [ f for f in filenameFilter if len(f)>0 and not f[0] == "!" ]
 
-    return file_list
+    for root, dirs, files in os.walk(datasetsPath):
+        # exclude dirs
+        dirs[:] = [os.path.join(root, d) for d in dirs]
+        dirs[:] = [d for d in dirs if not any(word in d for word in excludesDir)]
+        if len(includesDir) > 0:
+            dirs[:] = [d for d in dirs if any(word in d for word in includesDir)]
+            if not any(word in root for word in includesDir):
+                continue
+
+        # exclude/include files
+        files = [os.path.join(root, f) for f in files]
+        files = [f for f in files if not any(word in f for word in excludes)]
+        if len(excludes) > 0:
+            files = [f for f in files if not any(word in f for word in excludes)]
+        if len(includes) > 0:
+            files = [f for f in files if any(word in f for word in includes)]
+
+        for fname in files:
+            filtered_files.append(fname) 
+            dataFrames.append( LoadData(fname) )
+
+
+    baseDataPath = os.path.commonpath(filtered_files)
+    baseDataPath = os.path.dirname(baseDataPath)
+    filtered_files = [f.removeprefix(baseDataPath) for f in filtered_files]
+
+    filtered_files = [{ "label": filtered_files[i], "value": i } for i in range(len(filtered_files))]
+    return filtered_files
 
 @app.callback(
     Output("graph", "children", allow_duplicate=True),
     Output("table-content", "children"),
+    Output("xaxis", "options"),
+    Output("columns", "options"),
     Input('dataset-select', 'value'),
     Input('data-table', 'data'),
     Input('data-table', 'columns'),
     prevent_initial_call=True,
 )
 def UpdateTableSelectedDataset(datasetIndex, rows, columns):
+    global xAxis
+    global dataColumns
+
     changed_inputs = [
         x["prop_id"]
         for x in callback_context.triggered
     ]
 
     if "dataset-select.value" in changed_inputs:
+        if datasetIndex is None:
+            raise PreventUpdate
+
         dataFrameIndex = datasetIndex
         df = dataFrames[dataFrameIndex]
+        DF_KEYS = df.keys()
+        xAxis = DF_KEYS[0] if len(DF_KEYS) > 0 else ""
+        xAxisOptions=[{"label": DF_KEYS[x], "value":x} for x in range(len(DF_KEYS))]
+        dataColumns = [ key for key in DF_KEYS if key != xAxis]
+        columnsOptions=[{"label": DF_KEYS[x], "value":x} for x in range(len(DF_KEYS)) if x != xAxis]
+
         return [
             drawFigure(df),
-            drawTable(df)
+            drawTable(df),
+            xAxisOptions,
+            columnsOptions,
         ]
 
     if "data-table.data" in changed_inputs or "data-table.columns" in changed_inputs:
@@ -318,7 +407,9 @@ def UpdateTableSelectedDataset(datasetIndex, rows, columns):
         df = dataFrames[datasetIndex]
         return [
             drawFigure(df),
-            no_update
+            no_update,# drawTable(df),
+            no_update,#xAxisOptions,
+            no_update,#columnsOptions,
         ]
 
 #########################################################
